2ce98d0ce8bd69bfe5aa9b736b0a2cdbde4586a9
lucas <mr.pine97@gmail.com>
2021-08-04 21:12:12 -0300

    exercicio 3

---
+++ src/exercise3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class exercise3 {
+  public static void main(String[] args) {
+    Scanner doubleGrades = new Scanner(System.in);
+
+    System.out.print("digite a primeira nota: ");
+    double a = doubleGrades.nextDouble();
+
+    System.out.print("digite a segunda nota: ");
+    double b = doubleGrades.nextDouble();
+
+    System.out.print("digite a terceira nota: ");
+    double c = doubleGrades.nextDouble();
+
+    doubleGrades.close();
+
+    int peso1 = 5;
+    int peso2 = 3;
+    int peso3 = 2;
+
+
+    System.out.println("a média é : " + (((a * peso1) + (b * peso2) + (c * peso3))/(peso1 + peso2 + peso3)));
+  }
+}
