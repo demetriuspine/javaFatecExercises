00e931057c18f5fe632eed340a15429a8d90fca7
lucas <mr.pine97@gmail.com>
2021-08-07 23:24:49 -0300

    exercicio 9

---
+++ src/exercise9.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class exercicio9 {
+  public static void main(String[] args) {
+    Scanner trianglScanner = new Scanner(System.in);
+
+    System.out.print("Digite o valor da base do triângulo: ");
+    double base = trianglScanner.nextDouble();
+    
+    System.out.print("Digite o valor da altura do triângulo: ");
+    double height = trianglScanner.nextDouble();
+    
+    double area = (base * height)/2;
+
+    trianglScanner.close();
+
+    System.out.println("o valor da área do triângulo é de: " + area);
+  }
+}
